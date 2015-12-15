e98255a99b46558d1dad9f552b1c25091f33625b
Kamil Berdychowski <kamil.berdychowski@gmail.com>
2015-12-15 14:59:22 +0100

    Money object enhanced.

--- src/main/java/pl/pragmatists/atm/domain/Money.java
+++ src/main/java/pl/pragmatists/atm/domain/Money.java
@@ -1,11 +1,57 @@
 package pl.pragmatists.atm.domain;
 
-public class Money {
-    public final int dollars;
-    public final int cents;
+import static java.lang.String.format;
 
-    public Money(int dollars, int cents) {
-        this.dollars = dollars;
-        this.cents = cents;
+public class Money {
+    private final int decimal;
+    private final int factorial;
+
+    public Money(int decimal, int factorial) {
+        this.decimal = decimal;
+        this.factorial = factorial;
+    }
+
+    public Money subtract(int toWithdraw) {
+        return new Money(decimal - toWithdraw, factorial);
+    }
+
+    public Money add(int amount) {
+        return new Money(decimal + amount, factorial);
+    }
+
+    public boolean hasAtLeast(int amount) {
+        return decimal >= amount;
+    }
+
+    public int getDecimal() {
+        return decimal;
+    }
+
+    public int getFactorial() {
+        return factorial;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Money money = (Money) o;
+
+        if (decimal != money.decimal) return false;
+        return factorial == money.factorial;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = decimal;
+        result = 31 * result + factorial;
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return format("%d,%d", decimal, factorial);
     }
 }
